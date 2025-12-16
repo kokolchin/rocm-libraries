753c4c0ea394930c2a3deb97a09288e3ea3e839a
kokolchin <kokolchin@luxoft.com>
2025-12-16 12:15:13 +0100

    Apply clang format to pooling2d.cpp

--- projects/miopen/test/gtest/pooling2d.cpp
+++ projects/miopen/test/gtest/pooling2d.cpp
@@ -13,9 +13,9 @@ namespace {
 
 struct Pooling2dTestCase
 {
-    std::vector<int> input_dims;  // [N, C, H, W]
-    std::vector<int> lens;        // [H, W]
-    std::vector<int> pads;        // [H, W]
+    std::vector<int> input_dims; // [N, C, H, W]
+    std::vector<int> lens;       // [H, W]
+    std::vector<int> pads;       // [H, W]
     std::vector<int> strides;    // [H, W]
     miopenIndexType_t index_type;
     miopenPoolingMode_t mode;
@@ -27,8 +27,7 @@ struct Pooling2dTestCase
                   << ", lens: " << miopen::Join(tc.lens, ", ")
                   << ", pads: " << miopen::Join(tc.pads, ", ")
                   << ", strides: " << miopen::Join(tc.strides, ", ")
-                  << ", index_type: " << tc.index_type
-                  << ", mode: " << tc.mode
+                  << ", index_type: " << tc.index_type << ", mode: " << tc.mode
                   << ", wsidx: " << tc.wsidx;
     }
 };
@@ -44,7 +43,7 @@ std::vector<Pooling2dTestCase> GetPooling2dTestCases()
         {{10, 3, 32, 32}, {3, 3}, {1, 1}, {1, 1}, miopenIndexUint8, miopenPoolingAverage, 1},
         {{2, 64, 112, 112}, {2, 2}, {0, 0}, {2, 2}, miopenIndexUint8, miopenPoolingMax, 1},
         {{4, 3, 224, 224}, {3, 3}, {1, 1}, {1, 1}, miopenIndexUint8, miopenPoolingAverage, 0},
-        
+
         // Dataset 1: Minimal dataset (asymmetric configs, small tensors)
         {{1, 4, 4, 4}, {2, 2}, {0, 0}, {1, 1}, miopenIndexUint8, miopenPoolingMax, 0},
         {{1, 4, 4, 4}, {2, 2}, {0, 0}, {2, 2}, miopenIndexUint8, miopenPoolingAverage, 1},
@@ -52,10 +51,16 @@ std::vector<Pooling2dTestCase> GetPooling2dTestCases()
         {{1, 4, 4, 4}, {2, 1}, {0, 0}, {2, 1}, miopenIndexUint8, miopenPoolingAverage, 0},
         {{1, 4, 4, 4}, {2, 2}, {0, 0}, {1, 2}, miopenIndexUint8, miopenPoolingMax, 1},
         {{1, 4, 4, 4}, {2, 2}, {0, 0}, {2, 1}, miopenIndexUint8, miopenPoolingAverage, 0},
-        
+
         // Additional coverage: different index types and modes
         {{5, 32, 8, 8}, {2, 2}, {0, 0}, {2, 2}, miopenIndexUint32, miopenPoolingMax, 1},
-        {{5, 32, 8, 8}, {3, 3}, {1, 1}, {1, 1}, miopenIndexUint32, miopenPoolingAverageInclusive, 0},
+        {{5, 32, 8, 8},
+         {3, 3},
+         {1, 1},
+         {1, 1},
+         miopenIndexUint32,
+         miopenPoolingAverageInclusive,
+         0},
     };
 }
 
@@ -89,24 +94,23 @@ void RunPooling2dTestWithIndexType(const Pooling2dTestCase& test_case)
         if(test_case.lens[i] >
            (input_desc.GetLengths()[i + 2] + static_cast<uint64_t>(2) * test_case.pads[i]))
         {
-            GTEST_SKIP() << "Invalid config: lens[" << i
-                         << "] > (input_dims[" << i + 2 << "] + 2 * pads[" << i << "])";
+            GTEST_SKIP() << "Invalid config: lens[" << i << "] > (input_dims[" << i + 2
+                         << "] + 2 * pads[" << i << "])";
         }
     }
 
     // Run forward pooling
     std::vector<Index> indices;
     verify_forward_pooling<2> forward_verifier;
-    auto forward_result = forward_verifier.cpu(input, filter, indices);
+    auto forward_result     = forward_verifier.cpu(input, filter, indices);
     auto forward_gpu_result = forward_verifier.gpu(input, filter, indices);
 
     // Compare forward results
-    EXPECT_EQ(miopen::range_distance(forward_result),
-              miopen::range_distance(forward_gpu_result));
+    EXPECT_EQ(miopen::range_distance(forward_result), miopen::range_distance(forward_gpu_result));
 
-    using value_type = T;
-    const double tolerance = 80.0;
-    const double threshold = std::numeric_limits<value_type>::epsilon() * tolerance;
+    using value_type               = T;
+    const double tolerance         = 80.0;
+    const double threshold         = std::numeric_limits<value_type>::epsilon() * tolerance;
     const double forward_rms_error = miopen::rms_range(forward_result, forward_gpu_result);
 
     EXPECT_LE(forward_rms_error, threshold)
@@ -123,8 +127,7 @@ void RunPooling2dTestWithIndexType(const Pooling2dTestCase& test_case)
         input, dout, forward_result, filter, indices, test_case.wsidx != 0, true);
 
     // Compare backward results
-    EXPECT_EQ(miopen::range_distance(backward_result),
-              miopen::range_distance(backward_gpu_result));
+    EXPECT_EQ(miopen::range_distance(backward_result), miopen::range_distance(backward_gpu_result));
 
     const double backward_rms_error = miopen::rms_range(backward_result, backward_gpu_result);
 
@@ -191,15 +194,9 @@ class GPU_Pooling2d_FP16 : public testing::TestWithParam<Pooling2dTestCase>
     }
 };
 
-TEST_P(GPU_Pooling2d_FP32, FloatTest_pooling2d)
-{
-    RunPooling2dTest<float>(GetParam());
-}
+TEST_P(GPU_Pooling2d_FP32, FloatTest_pooling2d) { RunPooling2dTest<float>(GetParam()); }
 
-TEST_P(GPU_Pooling2d_FP16, HalfTest_pooling2d)
-{
-    RunPooling2dTest<half_float::half>(GetParam());
-}
+TEST_P(GPU_Pooling2d_FP16, HalfTest_pooling2d) { RunPooling2dTest<half_float::half>(GetParam()); }
 
 INSTANTIATE_TEST_SUITE_P(Smoke, GPU_Pooling2d_FP32, testing::ValuesIn(GetPooling2dTestCases()));
 
