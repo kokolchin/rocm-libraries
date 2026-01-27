1c123506645dfa32975de4acae94482ba41c0acb
kokolchin <kokolchin@luxoft.com>
2026-01-27 23:48:54 +0100

    Fix: Add constructor to tensor struct to allow initialization from descriptor and data

--- projects/miopen/test/tensor_holder.hpp
+++ projects/miopen/test/tensor_holder.hpp
@@ -154,7 +154,9 @@ struct tensor
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #endif
 
-    tensor() : desc(miopen_type<T>{}) {}
+    
+    tensor(const miopen::TensorDescriptor& d, const std::vector<T>& data_) : desc(d), data(data_) {}
+tensor() : desc(miopen_type<T>{}) {}
 
 #if defined(__clang__) || defined(__GNUG__)
 #pragma GCC diagnostic pop
