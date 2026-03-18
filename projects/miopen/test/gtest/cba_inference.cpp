a69c4fa30f9d29dfb69ffba7d2c790cc93805d26
kokolchin <kokolchin@luxoft.com>
2026-03-18 11:00:07 +0100

    Remove ConvWinoRageRxSf2x3Fused test - solver was removed from registry

--- projects/miopen/test/gtest/cba_inference.cpp
+++ projects/miopen/test/gtest/cba_inference.cpp
@@ -208,10 +208,7 @@ TEST_P(GPU_ConvBiasActivInfer_FP16, ConvWinoFuryRxSf2x3Fused)
 {
     RunSolver(miopen::solver::fusion::ConvWinoFuryRxSFused<2, 3>{});
 }
-TEST_P(GPU_ConvBiasActivInfer_FP16, ConvWinoRageRxSf2x3Fused)
-{
-    RunSolver(miopen::solver::fusion::ConvWinoRageRxSFused<2, 3>{});
-}
+// ConvWinoRageRxSFused was removed from the solver registry (solver.cpp)
 
 TEST_P(GPU_ConvBiasActivInfer_FP16, ConvCKIgemmFwdBiasActivFused)
 {
